afbaab767dc85cd33773b77ef2c7a02957d1b85a
denimar <denimar@gmail.com>
2017-05-08 17:14:31 -0300

    testing Heroky hosting...

--- bin/dev-server.js
+++ bin/dev-server.js
@@ -1,21 +1,15 @@
-console.log('-------------------')
-console.log(`PORT -> : ${process.env.PORT}`);
-console.log('-------------------')
-
 const project = require('../config/project.config')
 const server = require('../server/main')
 const debug = require('debug')('app:bin:dev-server')
 
-server.listen(process.env.PORT || 3000, function(err) {
-  if (err) {
-    debug(`Error serving ion port ${process.env.PORT}.`)
-    return;
-  }
-  debug(`Server is now running on port ${process.env.PORT}.`)
-})
 
-setInterval(function() {
-  console.log('-------------------')
-  console.log(`PORT -> : ${process.env.PORT}`);
-  console.log('-------------------')
-}, 10000)
+//For avoidong Heroku $PORT error
+server.get('/', function(request, response) {
+    response.send('App is running...');
+}).listen(process.env.PORT || 3000, function(err) {
+	console.log('****************************************')
+	console.log(err)
+	console.log('****************************************')
+	
+	  debug(`Server is now running on :${process.env.PORT}.`)
+})
