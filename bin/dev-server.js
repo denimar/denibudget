348d3ac9b28016cc508eae1b2b9f8ac97a3f4e7b
denimar <denimar@gmail.com>
2017-05-08 18:28:40 -0300

    testing Heroky hosting...

--- bin/dev-server.js
+++ bin/dev-server.js
@@ -5,11 +5,6 @@ const path = require('path');
 
 //For avoidong Heroku $PORT error
 server.get('/', function(request, response) {
-	console.log('+++++++++++++++++++++++++++++')
-	console.log(path.join(__dirname + './index.html'))
-    console.log('+++++++++++++++++++++++++++++')
-	
-	response.sendFile(path.join(__dirname + './index.html'));
 
 }).listen(process.env.PORT || 3000, function(err) {
 	console.log('****************************************')
