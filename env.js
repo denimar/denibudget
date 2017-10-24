67ff8d3e1225644bd1bc05a0c5508ce678d4f6d2
denimar <denimar.moraes@wplex.com.br>
2017-10-24 10:35:34 -0200

    changed database host

--- env.js
+++ env.js
@@ -20,6 +20,17 @@ module.exports = {
 
   production: {
     //PORT: 3000,
+    MONGODB_HOST: '32-3a.mongo.evennode.com',
+    MONGODB_PORT: 27017,
+    MONGODB_DATABASE: 'b06782f406525ca8f6285931fb0fb06a',
+    MONGODB_CONNECTION_TIMEOUT: 10000,
+    MONGODB_REPLICA_SET: true,
+    MONGODB_USER: 'b06782f406525ca8f6285931fb0fb06a',
+    MONGODB_PASSWORD: 'dm90460'
+  }
+/*
+  production: {
+    //PORT: 3000,
     MONGODB_HOST: 'ds117109.mlab.com',
     MONGODB_PORT: 17109,
     MONGODB_DATABASE: 'budget',
@@ -28,5 +39,6 @@ module.exports = {
     MONGODB_USER: 'denimar',
     MONGODB_PASSWORD: 'dm90460'
   }
+*/
 
 }
