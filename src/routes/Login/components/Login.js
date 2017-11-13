ad12ff61ea2d306a2dfcdab387b300bdaa3de6c2
denimar <denimar.moraes@wplex.com.br>
2017-11-13 15:17:13 -0200

    partial commit

--- src/routes/Login/components/Login.js
+++ src/routes/Login/components/Login.js
@@ -5,7 +5,6 @@ import TiUserOutline from 'react-icons/lib/ti/user-outline';
 import LoginService from '../modules/LoginService';
 import AuthenticationRoutine from '../../../util/AuthenticationRoutine';
 import 'bootstrap/dist/css/bootstrap.css'
-import Dialog from 'react-bootstrap-dialog';
 
 class Login extends React.Component {
 
@@ -20,39 +19,66 @@ class Login extends React.Component {
     this.clearForm();
   }
 
+  getFormValues() {
+    let loginForm = document.querySelector('#login-form');
+    let nickName = loginForm.querySelector('input[name=userName]').value;
+    let password = loginForm.querySelector('input[name=password]').value;
+    return {
+      nickName: nickName,
+      password: password
+    }
+  }
+
   clearForm() {
     let loginForm = document.querySelector('#login-form');
     let nickNameInput = loginForm.querySelector('input[name=userName]');
     nickNameInput.value = null;
-
-    setTimeout(() => {
-      nickNameInput.focus();
-    }, 500);
-
     let passwordInput = loginForm.querySelector('input[name=password]');
     passwordInput.value = null;
+
+    nickNameInput.focus();    
+  }
+
+  checkDisableLoginButton() {
+    let form = this.getFormValues();
+    let loginButton = document.querySelector('#login-button');
+    loginButton.classList.remove('disabled');
+    if (!(form.nickName && form.password)) {
+      loginButton.classList.add('disabled');
+    }
+  }
+
+  showMessage(message) {
+    let loginMessages = document.querySelector('#login-messages');
+    loginMessages.innerHTML = message;
+    loginMessages.style.display = 'block';
+  }
+
+  onChangeInputs() {
+    this.checkDisableLoginButton();
+  }
+
+  onKeydownInputs(event) {
+    if (event.keyCode === 13) {
+      let loginButton = document.querySelector('#login-button');
+      let isLoginButtonDisable = loginButton.classList.contains('disabled');
+      if (!isLoginButtonDisable) {
+        this.loginButtonClick.call(this);
+      }
+    }
   }
 
   loginButtonClick() {
-    let loginForm = document.querySelector('#login-form');
-    let nickName = loginForm.querySelector('input[name=userName]').value;
-    let password = loginForm.querySelector('input[name=password]').value;
+    let form = this.getFormValues();
 
     LoginService
-      .authenticate(nickName, password)
+      .authenticate(form.nickName, form.password)
       .then((response) => {
         if (response.success) {
           window.location = "/budget";
         } else {
-          this.refs.dialog.show({
-            title: 'Login',
-            body: I18n.t('login.authentication.failureMsg'),
-            actions: [
-              Dialog.DefaultAction('Ok', () => {
-                this.clearForm();
-              })
-            ]
-          })
+          this.showMessage(I18n.t('login.authentication.failureMsg'));
+          this.clearForm();
         }
       });
   }
@@ -60,32 +86,34 @@ class Login extends React.Component {
   render() {
     return (
       <div className="login-viewport">
-        <Dialog ref='dialog'/>
         <div className="login-container" style={{display: "none"}}>
-          <div className="login-box">
-            <div className="login-title">Login - Budget</div>
-            <div className="login-image">
-              <TiUserOutline size="64" />
-            </div>
+          <div className="login-box-and-messages">
+            <div className="login-box">
+              <div className="login-title">Login - Budget</div>
+              <div className="login-image">
+                <TiUserOutline size="64" />
+              </div>
 
-            <form id="login-form">
-              <div className="login-field">
-                <select className="login-field-input">
-                  <option value="oficial">Base Oficial on MapLab</option>
-                  <option value="teste">Base Teste on MapLab</option>
-                </select>
-              </div>
-              <div className="login-field">
-                <input type="text" name="userName" className="login-field-input" placeholder={I18n.t('login.user')} />
-              </div>
-              <div className="login-field">
-                <input type="password" name="password" className="login-field-input" placeholder={I18n.t('login.password')} />
-              </div>
-            </form>
+              <form id="login-form">
+                <div className="login-field">
+                  <select className="login-field-input">
+                    <option value="oficial">Base Oficial on MapLab</option>
+                    <option value="teste">Base Teste on MapLab</option>
+                  </select>
+                </div>
+                <div className="login-field">
+                  <input type="text" name="userName" className="login-field-input" onChange={this.onChangeInputs.bind(this)} onKeyDown={this.onKeydownInputs.bind(this)} placeholder={I18n.t('login.user')} />
+                </div>
+                <div className="login-field">
+                  <input type="password" name="password" className="login-field-input" onChange={this.onChangeInputs.bind(this)} onKeyDown={this.onKeydownInputs.bind(this)} placeholder={I18n.t('login.password')} />
+                </div>
+              </form>
 
-            <div className="login-button" onClick = {this.loginButtonClick.bind(this)}>
-              <Translate value="login.buttonText" />
+              <div id="login-button" className="login-button disabled" onClick = {this.loginButtonClick.bind(this)}>
+                <Translate value="login.buttonText" />
+              </div>
             </div>
+            <div id="login-messages" style={{display: 'none'}} className="login-messages" />
           </div>
         </div>
       </div>
